c51f9a1eab7aa07c81205aa39edd31775e6500e1
James Smith <james@floppy.org.uk>
2010-09-30 15:55:20 +0100

    Remove hardcoded data item UIDs, that's far from best practise! MA-103

--- lib/amee/methodology.rb
+++ lib/amee/methodology.rb
@@ -4,50 +4,54 @@ module AMEE
         
     # http://explorer.amee.com/categories/Generic_plane_transport        
     def self.flight type, origin, destination
-      category = "/transport/plane/generic/"
-      data_item_id = data_item_id category / type
-            
+      category = "/transport/plane/generic"
+      drills={
+        "auto_one_way" => "type=auto&size=one+way",
+        "auto_return" => "type=auto&size=return",
+      }
+      data_item_id = uid(category, drills[type])
       params = {
         "IATACode1" => origin,
         "IATACode2" => destination,
       }
-      
       [category, data_item_id, params]
     end
       
     # http://explorer.amee.com/categories/Generic_plane_transport    
     def self.flight_aggregator distance, count
-      category = "/transport/plane/generic/"
-      data_item_id = data_item_id category / distance
-      
+      category = "/transport/plane/generic"
+      drills={
+        "domestic_return" => "type=domestic&size=return",
+        "long_haul_return" => "type=long+haul&size=return",
+        "short_haul_return" => "type=short+haul&size=return",
+      }
+      data_item_id = uid(category, drills[distance])
       params = {
         :journeys => count
       }
-      
       [category, data_item_id, params]
     end
     
     # http://explorer.amee.com/categories/Uk_energy_by_price
     def self.energy_by_cost energy_type, quantity 
-      if energy_type == "electricity" || energy_type == "gas"
-        energy_type << "_normal" 
-      end
-      
-      category = "/home/energy/uk/price/"
-      data_item_id = data_item_id category / energy_type
-      
+      category = "/home/energy/uk/price"
+      drills = {
+        "coal" => "type=coal&payment=-",
+        "electricity" => "type=electricity&payment=normal",
+        "gas" => "type=gas&payment=normal",
+        "oil" => "type=oil&payment=-",
+      }
+      data_item_id = uid(category, drills[energy_type])
       params = {
         :currencyGBP => quantity
       }
-      
       [category, data_item_id, params]
     end
     
-    # http://explorer.amee.com/categories/Energy_by_Quantity/data/electricity
+    # http://explorer.amee.com/categories/Energy_by_Quantity
     def self.energy_by_quantity energy_type, quantity   
       category = "/home/energy/quantity"
-      data_item_id = data_item_id category / energy_type
-
+      data_item_id = uid(category, "type=#{energy_type}")
       param_key = {
         "electricity" => :energyConsumption,
         "gas" => :energyConsumption,
@@ -57,27 +61,24 @@ module AMEE
       params = {
         param_key => quantity
       }
-      
       [category, data_item_id, params]
     end
     
     # http://explorer.amee.com/categories/Generic_car_transport
     def self.car size, fuel_type, distance, distance_unit 
       category = "/transport/car/generic"
-      data_item_id = data_item_id category / "#{fuel_type}_#{size}"
-
+      data_item_id = uid(category, "fuel=#{fuel_type.gsub('_','+')}&size=#{size}")
       params = {
         :distance => distance,
         :distanceUnit => distance_unit
       }
-      
       [category, data_item_id, params]
     end
     
+    # http://explorer.amee.com/categories/Generic_bus_transport
     def self.bus distance, distance_unit, journey_count
       category = "/transport/bus/generic"
-      data_item_id = data_item_id category / "typical"
-      
+      data_item_id = uid(category, "type=typical")
       params = {
         :distancePerJourney => distance,
         :distancePerJourneyUnit => distance_unit,
@@ -88,81 +89,52 @@ module AMEE
       [category, data_item_id, params]
     end
     
+    # http://explorer.amee.com/categories/Generic_train_transport
     def self.train distance, distance_unit, journey_count
       category = "/transport/train/generic"
-      data_item_id = data_item_id category / "national"
-      
+      data_item_id = uid(category, "type=national")
       params = {
         :distancePerJourney => distance,
         :distancePerJourneyUnit => distance_unit,
         :numberOfJourneys => journey_count,
         :journeyFrequency => "yearly"
       }
-      
       [category, data_item_id, params]
     end
     
     # http://explorer.amee.com/categories/Freight
     def self.freight type, dist, dist_unit, mass, mass_unit
       category = "/transport/byDistance/freight"
-      data_item_id = data_item_id category / type
-      
+      drills={
+        "Plane_Long_haul" => "type=plane&subtype=long+haul",
+        "Plane_Medium_haul" => "type=plane&subtype=medium+haul",
+        "Plane_Short_haul" => "type=plane&subtype=short+haul",
+        "Road_General" => "type=road&subtype=general",
+        "Ship_Inland" => "type=ship&subtype=inland",
+        "Ship_Marine" => "type=ship&subtype=marine",
+        "Train_Coal" => "type=train&subtype=coal",
+        "Train_Diesel" => "type=train&subtype=diesel",
+        "Train_Electric" => "type=train&subtype=electric",
+      }
+      data_item_id = uid(category, drills[type])
       params = {
         :distance => dist,
         :distanceUnit => dist_unit,
         :mass => mass,
         :massUnit => mass_unit,
       }
-      
       [category, data_item_id, params]
     end
-                
+
+    def self.uid(category, drills)
+      AMEE::DataItemCache.get(AMEE::Rails.connection, "#{category}/drill?#{drills}")
+    end
+
     def self.delete amee_cx, profile_id, item
       path = "/profiles" / profile_id / item.path
       AMEE::Profile::Item.delete amee_cx, path
     end
-    
-    def self.categories
-      %w(
-        /transport/plane/generic
-        /transport/car/generic
-        /home/energy/quantity
-        /home/energy/uk/price
-        /transport/byDistance/freight
-        /transport/bus/generic
-        /transport/train/generic
-      )
-    end
-    
-    def self.data_item_id key
-      server = AMEE::Rails.connection.server.gsub('.','_')
-      AMEE::DataItemCache.send server, key
-    end
-    
-    #
-    # Ideally, this method would dynamically define the cache method on
-    # initialization in production. For other environments, the script would be
-    # run manually and the string returned written to a file. However, given
-    # the possibility of timeouts, I feel it's more prudent to statically 
-    # create the cache for all environments.
-    #
-    def self.create_data_item_cache amee_cx
-      server = AMEE::Rails.connection.server.gsub('.','_')
-
-      m_def = "def self.#{server} key\n  {\n"
-      
-      categories.each do |cat|
-        r = amee_cx.get "/data" / cat + "?itemsPerPage=999"
-        r = JSON.parse(r.body).to_openstruct
-        r.children.dataItems.rows.each do |r|
-          tag = r.label.gsub(", ", "_").gsub(" ", "_")
-          m_def << %Q{    "#{cat}/#{tag}" => "#{r.uid}",\n}
-        end
-      end
-      
-      m_def << "  }[key]\nend"
-    end
-                
+          
   end
   
 end
